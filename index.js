0324ffb94d576d2af6347d88806c59afa41690b4
Josiah Powell <josiahpowell@outlook.com>
2020-08-06 22:34:19 -0500

    Adding pseudocode

---
+++ index.js
@@ -0,0 +1,17 @@
+var inquirer = require('inquirer');
+
+// TODO: Generate questions and validation for ...
+
+// Title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+
+
+// TODO: 
+// Generate readme.md 
