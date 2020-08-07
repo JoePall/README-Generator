52710da4423ec7ab77ea613dc9aa8b4fa3cf3802
Josiah Powell <josiahpowell@outlook.com>
2020-08-07 14:03:01 -0500

    Adding prompt functionality

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,19 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+    
+    // Title
+    // Description
+    // Table of Contents
+    // Installation
+    // Usage
+    // License
+    // Contributing
+    // Tests
+    // Questions
+
+    return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
