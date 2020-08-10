ccfea714f512dd2209e5a027051e5e84507d0a92
Josiah Powell <josiahpowell@outlook.com>
2020-08-10 18:11:18 -0500

    Adding generateMarkdown Module

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,30 @@
+const { makeBadge, ValidationError } = require('badge-maker');
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+    console.log("generateMarkdown: started");
+    let { title, description, tableOfContents, installation, usage, license, contributing, tests, questions } = data;
+
+    console.log("makeBadge: started");
+    const licenseBadge = makeBadge({
+        label: 'license',
+        message: license,
+        color: 'green',
+    });
+    console.log("makeBadge: ended");
+
+    console.log("generateMarkdown: ended");
+    return `${licenseBadge}
     
-    // Title
-    // Description
-    // Table of Contents
-    // Installation
-    // Usage
-    // License
-    // Contributing
-    // Tests
-    // Questions
-
-    return `# ${data.title}
-
-`;
+# ${title}
+    
+    ${description}
+    
+${installation !== "" ? ("## Installation" + "\n\n\t" + installation) + "\n" : ""}
+${usage !== "" ? ("## Usage" + "\n\n\t" + usage) + "\n" : ""}
+${tests !== "" ? ("## Tests" + "\n\n\t" + tests) + "\n" : ""}
+${questions !== "" ? ("## Questions" + "\n\n\t" + questions) + "\n" : ""}
+${contributing !== "" ? ("## Contributing" + "\n\n\t" + contributing) : ""}`;
 }
 
 module.exports = generateMarkdown;
