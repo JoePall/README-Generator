cf18570bae1e2093f21618e2659bc2875316c0fa
Josiah Powell <josiahpowell@outlook.com>
2020-08-10 21:22:51 -0500

    Formatting readme.md

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const fs = require('fs');
-const { prompt } = require('inquirer');
+const inquirer = require('inquirer');
 const util = require("util");
 const generateMarkdown = require('./utils/generateMarkdown');
-//import generateMarkdown from "./utils/generateMarkdown.js";
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// array of questions for user
 const questions = [
     {
         type: 'input',
@@ -26,24 +13,14 @@ const questions = [
         message: 'Title: ',
     },
     {
-        type: 'editor',
-        name: 'description',
-        message: 'Description: ',
+        type: 'input',
+        name: 'github',
+        message: 'GitHub Username: ',
     },
     {
-        type: 'editor',
-        name: 'tableOfContents',
-        message: 'Table of Contents: ',
-    },
-    {
-        type: 'editor',
-        name: 'installation',
-        message: 'Installation: ',
-    },
-    {
-        type: 'editor',
-        name: 'usage',
-        message: 'Usage: ',
+        type: 'input',
+        name: 'repo',
+        message: 'Repository name: ',
     },
     {
         type: 'list',
@@ -68,6 +45,21 @@ const questions = [
     },
     {
         type: 'editor',
+        name: 'description',
+        message: 'Description: ',
+    },
+    {
+        type: 'editor',
+        name: 'installation',
+        message: 'Installation: ',
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Usage: ',
+    },
+    {
+        type: 'editor',
         name: 'contributing',
         message: 'Contributing: ',
     },
@@ -87,12 +79,12 @@ const questions = [
 function writeToFile(fileName, data) {
     writeFileAsync(fileName, data).then(() => {
         console.log("Readme.md written!");
-    });    
+    });
 }
 
 // function to initialize program
 function init() {
-    prompt(questions).then(response => {
+    inquirer.prompt(questions).then(response => {
         var markdown = generateMarkdown(response);
         writeToFile("readme.md", markdown);
     });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,25 +2,34 @@ const { makeBadge, ValidationError } = require('badge-maker');
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-    let { title, description, tableOfContents, installation, usage, license, contributing, tests, questions } = data;
-
+    let { title, description, github, repo, installation, usage, license, contributing, tests, questions } = data;
+    
     const licenseBadge = makeBadge({
         label: 'license',
         message: license,
-        color: 'red',
+        color: '#322200',
     });
 
-    return `${licenseBadge}
-    
-# ${title}
-    
-    ${description}
-    
-${installation !== "" ? ("## Installation" + "\n\n\t" + installation) + "\n" : ""}
-${usage !== "" ? ("## Usage" + "\n\n\t" + usage) + "\n" : ""}
-${tests !== "" ? ("## Tests" + "\n\n\t" + tests) + "\n" : ""}
-${questions !== "" ? ("## Questions" + "\n\n\t" + questions) + "\n" : ""}
-${contributing !== "" ? ("## Contributing" + "\n\n\t" + contributing) : ""}`;
+    const featuresBadge = makeBadge({
+        message: 'features',
+        color: '#1e2b0d',
+    });
+
+    const issuesBadge = makeBadge({
+        message: 'issues',
+        color: '#332013',
+    });
+
+    console.log(description.length);
+
+    return `<a href="https://github.com/${github}/${repo}/issues?q=is%3Aopen+is%3Aissue+label%3Afeature-request+sort%3Areactions-%2B1-desc">${featuresBadge}</a><a href="https://github.com/${github}/${repo}/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+label%3Abug">${issuesBadge}</a>${licenseBadge}
+<h1>${title}</h1>
+${description.length != 0 ? "## Description" + "\n<p>" + description + "</p>\n\n" : ""}${installation.length != 0 ? "## Installation" + "\n<p>" + installation + "</p>\n\n" : ""}${usage.length != 0 ? "## Usage" + "\n<p>" + usage + "</p>\n\n" : ""}${tests.length != 0 ? "## Tests" + "\n<p>" + tests + "</p>\n\n" : ""}${questions.length != 0 ? "## Questions" + "\n<p>" + questions + "</p>\n\n" : ""}${contributing.length != 0 ? "## Contributing" + "\n<p>" + contributing + "</p>\n\n" : ""}${license.length != 0 ? "## License" + "\n<p>" + license + "</p>\n\n" : ""}
+`;
 }
+
+
+    
+
 
 module.exports = generateMarkdown;
