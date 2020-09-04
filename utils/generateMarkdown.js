bafd479c1407b44e5a8e8b2d996322a7eacfe57e
Josiah Powell <josiahpowell@outlook.com>
2020-09-04 00:21:11 -0500

    Fixed shield issue

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,8 +19,8 @@ function generateMarkdown(data) {
     tableOfContents += `<!--te-->`;
 
 
-    return `![Enhancements](https://img.shields.io/github/issues/${github}/${repo}/enhancement.svg)](https://github.com/${github}/${repo}/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement+sort%3Areactions-%2B1-desc)
-![Bugs](https://img.shields.io/github/issues/${github}/${repo}/bug.svg)](https://github.com/${github}/${repo}/issues?utf8=✓&q=is%3Aissue+is%3Aopen+label%3Abug)
+    return `[![Enhancements](https://img.shields.io/github/issues/${github}/${repo}/enhancement.svg)](https://github.com/${github}/${repo}/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement+sort%3Areactions-%2B1-desc)
+[![Bugs](https://img.shields.io/github/issues/${github}/${repo}/bug.svg)](https://github.com/${github}/${repo}/issues?utf8=✓&q=is%3Aissue+is%3Aopen+label%3Abug)
 ![License](https://img.shields.io/badge/License-${license.replace(" ", "%20")}-green?style=flat-square.svg)
 
 # ${title}
